1ff797b9cacbce7bdee145b5385f408c11033a23
Martin Laws <martinblaws@gmail.com>
2019-04-16 21:38:55 -0400

    Added AJAX call for dog photos

--- app.js
+++ app.js
@@ -5,4 +5,22 @@ $(document).ready(() => {
   setTimeout(() => {
     $('.title').html(title);
   }, 1000)
+
+  $.ajax({
+    url: 'https://www.reddit.com/r/dogpictures.json'
+  })
+  .done(data => {
+    data.data.children.forEach((dogPhoto) => {
+      const imgTag = document.createElement('img')
+      imgTag.src = dogPhoto.data.thumbnail
+      
+      const imgWrapper = document.createElement('a')
+      imgWrapper.href = `http://reddit.com${dogPhoto.data.permalink}`
+      imgWrapper.target = '_blank'
+      $(imgWrapper).html(imgTag)
+      // console.log(imgTag, dogPhoto)
+      $('.content').append(imgWrapper)
+    })
+    console.log(data.data.children)
+  })
 })
